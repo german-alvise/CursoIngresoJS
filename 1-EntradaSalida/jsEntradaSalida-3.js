1311c820ac7bff013a418fd905dc7f6f647109fd
german-alvise <52534580+german-alvise@users.noreply.github.com>
2019-07-04 12:14:26 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,6 +2,16 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
+    var dato
+     dato = document.getelementbyid("elNombre").value;
+      alert(dato);
+
+
+
+
+
+
+
 
 
 }
