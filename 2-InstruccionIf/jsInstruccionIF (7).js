8ff9a73d1ef1c443e714fffc265ba6f9c6b996dc
german-alvise <52534580+german-alvise@users.noreply.github.com>
2019-07-19 11:36:09 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -4,8 +4,18 @@ function mostrar() {
   //tomo la edad
   var edad;
   edad = document.getElementById("edad").value;
-  if (edad < 18) {
-    alert("es muy pequeño para no ser soltero");
+
+  var estCivil;
+  estCivil = document.getElementById("estadoCivil").value;
+
+  if (edad < 18 && estCivil != "Soltero") {
+    alert("usted es muy pequeño para NO ser soltero");
   }
 
 }//FIN DE LA FUNCIÓN
+
+/*var edad;
+  edad = document.getElementById("edad").value;
+  if (edad < 18) {
+    alert("es muy pequeño para no ser soltero");
+  }*/
