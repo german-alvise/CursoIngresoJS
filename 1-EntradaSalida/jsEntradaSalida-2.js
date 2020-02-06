a19202382708441dd7006ea7aa8f2f97f3da0bfe
german-alvise <52534580+german-alvise@users.noreply.github.com>
2020-02-06 11:53:29 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,10 +1,19 @@
 /*Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-    var dato
+{	
+	var nombre;
+	var mensaje="ingrese su nombre";
+	nombre = prompt(mensaje);
+
+	//alert("su nombre es "       +     nombre);
+	console.log("su nombre es" +nombre);
+	console.log("su nombre es"+     nombre);
+	console.log("su nombre es "+nombre);
+	//alert(nombre);
+    /*var dato
         dato = prompt("dato");
-        alert(dato)
+        alert(dato);*/
 
 
 }
