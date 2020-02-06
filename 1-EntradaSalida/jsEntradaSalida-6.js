6de8f9e73688bd07cb99626c5a69357df8d5b24e
german-alvise <52534580+german-alvise@users.noreply.github.com>
2020-02-06 11:15:01 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -6,8 +6,11 @@ function sumar()
   var num1
   var num2
 
-  num1=prompt("ingrese un numero");//"6"
-  num2=prompt("ingrese un numero");//"6"
+  	num1=document.getElementById("numeroUno").value;
+  	num2=document.getElementById("numeroDos").value;
+
+  //num1=prompt("ingrese un numero");//"6"
+  //num2=prompt("ingrese un numero");//"6"
 
   num1=parseInt(num1);//"6"
   num2=parseInt(num2);
