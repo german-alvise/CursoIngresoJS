946434866d817ae02b1c1c96ce9f08a5563098bc
german-alvise <52534580+german-alvise@users.noreply.github.com>
2020-02-06 11:13:16 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,16 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{	var primerPrecio;
+	var segundoPrecio;
+	var apellido="nadie";
+
+	apellido="perez";
+	alert(apellido);
+		//alert("Esto funciona de maravilla");//valor entre doble comillas es literal
+		//alert(apellido);//valor de una variable
+		//alert("apellido");valor literal
+		//alert();sale cartel vacio
+		//alert(primerPre);si la variable no existe no podemos hacer nada 
+}
+
+/*alert recibe solo valores literales o variables*/
