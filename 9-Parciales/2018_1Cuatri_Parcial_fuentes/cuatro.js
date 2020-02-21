1ee7d08577fe6bdb8e2cf79c3f13b260aaaf6748
german-alvise <52534580+german-alvise@users.noreply.github.com>
2020-02-21 12:21:02 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,20 @@
-function mostrar()
-{
+function mostrar() {/*Pedir dos números y mostrar el resultado:
+Si son iguales los muestro concatenados.
+Si el primero es mayor, los resto,
+de lo contrario los sumo.
+Si la suma es mayor a 10 ,además de mostrar el resultado, muestro el mensaje
+"la suma es xxx y supero el 10".*/
+
+    var numeroIngresado1;
+    var numeroIngresado2;
+    var concatenados;
+
+    if (numeroIngresado1 == numeroIngresado2) {
+       concatenados= numeroIngresado1 + numeroIngresado2;
+        console.log(concatenados);
+    } else {
+            if(numeroIngresado1>numeroIngresado2)
+            numeroIngresado1-numeroIngresado2;
+    }
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,13 @@
 
 function mostrar()
 {
+    var altura;
+    var largo;
+    var perimetro;
 
+    altura=prompt("ingrese la altura del rectangulo"); 
+    largo=prompt("ingrese el largo del rectangulo");
+
+    perimetro=altura *2 +largo *2;
+    alert(perimetro);
 }
