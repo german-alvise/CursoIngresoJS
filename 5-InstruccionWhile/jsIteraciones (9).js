64c605ba885ac6a5a33582d40484b3a03345efa3
german-alvise <52534580+german-alvise@users.noreply.github.com>
2020-02-22 12:27:19 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,13 +1,53 @@
 /*Al presionar el botón pedir números hasta que el usuario quiera
 , mostrar el número máximo y el número mínimo ingresado.*/
 function mostrar() {
+	var contador = 0;
 	var numero;
+	var respuesta = true;
+	var maximo;
+	var minimo;
+
+	while (respuesta == true) {
+		numero = prompt("ingrese un numero:");
+		numero = parseInt(numero);
+		//respuesta.toLowerCase(respuesta);pasa todo a minusculas.
+		//alert(respuesta);
+		if (contador == 0) {
+			maximo = numero;
+			minimo = numero;
+		}
+		else {
+			if (numero > maximo) {
+				maximo = numero;
+			} else {
+				if (numero < maximo) {
+					minimo = numero;
+				}
+			}
+
+		}
+		contador++;
+		respuesta = confirm("¿desea ingresar otro numero?");
+	}
+
+	document.getElementById('maximo').value = maximo;
+	document.getElementById('minimo').value = minimo;
+
+
+
+
+
+
+
+
+
+	/*	var numero;
 	var maximo;
 	var minimo;
 	var primeraVez = true;
-	var respuesta = true;
+	var respuesta = "si";
 
-	while (respuesta) {
+	while (respuesta=="si") {
 		do {
 			numero = prompt("ingrese un numero");
 			numero = parseInt(numero);
@@ -16,21 +56,24 @@ function mostrar() {
 				primeraVez = false;
 				minimo = numero
 				maximo = numero
-			} else {
+			}
+			else {
 				if (numero > maximo) {
 					maximo = numero;
-				} else {
+				}
+				else {
 					if (numero < minimo) {
 						minimo = numero;
 					}
 				}
 			}
-			respuesta = confirm("¿desea continuar?");
+			respuesta = prompt("ingrese si para ingresar otro numero");
 		}
 
-		document.getElementById("maximo").value = maximo;
-		document.getElementById("minimo").value = minimo;
+		
 
 
 	}
+	document.getElementById("maximo").value = maximo;
+	document.getElementById("minimo").value = minimo;*/
 }//FIN DE LA FUNCIÓN
