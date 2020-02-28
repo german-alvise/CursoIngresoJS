9890f2abcb2756c51f0a3b26d8ea376cf927d458
german-alvise <52534580+german-alvise@users.noreply.github.com>
2020-02-28 10:06:29 -0300

    Add files via upload

---
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/nueve.js
@@ -0,0 +1,18 @@
+function mostrar() {
+    var nombre;
+    var edad;
+    var sexo;
+    var temperatura;
+
+nombre=prompt("ingrese su nombre");
+edad=prompt("ingrese su edad");
+edad=parseInt(edad);
+sexo=prompt("ingrese su sexo");
+temperatura=prompt("ingrese la temperatura");
+
+
+
+
+
+
+}
